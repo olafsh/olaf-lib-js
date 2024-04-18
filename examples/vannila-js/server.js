ebe333e7ad0c0a891bf59af7a6626be227a5d51e
Daniel Jaušovec <jausovec.daniel@gmail.com>
2024-04-18 18:43:57 +0200

    feat: multilanguage support (#29)
    * Update README.md
    
    * Initial commit
    
    * Update license, Rename package name
    
    * Remove .idea directory
    
    * Add modules scaffold, Update README.md, Add module usage examples
    
    * Rework module system, Add sample tests
    
    * Add pull request template
    
    * Add prettier support
    
    * Add Husky & Implement pre-commit hook
    
    * build: add convention commits commit-msg hook
    
    * build: add Github actions for tests & PR title check
    
    * build: add semantic release build pipeline
    
    * ci: trigger build
    
    * build: update node-version matrix
    
    * feat: implement existing SDK code
    
    * docs: update README.md
    
    * Update README.md
    
    * Update README.md
    
    * docs: update wording
    
    * docs: update overview section
    
    * docs: update initialize section
    
    * chore: bump version to 1.0.0 (#23)
    
    * feat: add language to build authorize url and update examples (#28)
    
    * feat: add language to build authorize url and update examples
    
    * chore: bump version to 1.0.1
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: Matej Hočevar <matej.hocevar@tretton37.com>
    Co-authored-by: Matej Hočevar <3007189+matejhocevar@users.noreply.github.com>

--- examples/vannila-js/server.js
+++ examples/vannila-js/server.js
@@ -1,39 +1,40 @@
-import {readFileSync} from "node:fs";
-import {createServer} from "node:https";
+import { readFileSync } from 'node:fs';
+import { createServer } from 'node:https';
 
-const key = readFileSync("./key.pem");
-const cert = readFileSync("./cert.pem");
+const key = readFileSync('./key.pem');
+const cert = readFileSync('./cert.pem');
 
-const httpsServer = createServer({
-  key,
-  cert
-}, (req, res) => {
-  console.log(req.method, req.url);
-  if (req.method === "GET") {
-    let content;
-    if (["", "/"].includes(req.url)) {
-      content = readFileSync("./public/index.html");
-    } else if (req.url === "/index.js") {
-      content = readFileSync("./public/index.js");
-    } else if (req.url === "/authorize.js") {
-      content = readFileSync("./public/authorize.js");
-    } else if (req.url.startsWith("/authorize")) {
-      content = readFileSync("./public/authorize.html");
+const httpsServer = createServer(
+  {
+    key,
+    cert,
+  },
+  (req, res) => {
+    if (req.method === 'GET') {
+      let content;
+      if (['', '/'].includes(req.url)) {
+        content = readFileSync('./public/index.html');
+      } else if (req.url === '/index.js') {
+        content = readFileSync('./public/index.js');
+      } else if (req.url === '/authorize.js') {
+        content = readFileSync('./public/authorize.js');
+      } else if (req.url.startsWith('/authorize')) {
+        content = readFileSync('./public/authorize.html');
+      } else {
+        res.writeHead(404).end();
+        return;
+      }
+
+      res.writeHead(200, {
+        'content-type': req.url.includes('.js') ? 'text/javascript' : 'text/html',
+      });
+      res.write(content);
+      res.end();
     } else {
       res.writeHead(404).end();
-      return;
     }
-
-    res.writeHead(200, {
-      "content-type": req.url.includes(".js") ? "text/javascript" : "text/html"
-    });
-    res.write(content);
-    res.end();
-  } else {
-    res.writeHead(404).end();
   }
-});
-
+);
 
 const host = process.env.HOST;
 const port = process.env.PORT || 3000;
